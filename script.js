1f1a0b58436be20b06658107ebd72ca3e3906f68
Danielle-design-art <daniellemorvant1@gmail.com>
2025-04-24 19:59:41 -0500

    added 3 files and starter content to each

---
+++ script.js
@@ -0,0 +1,21 @@
+let songs = [
+
+    {
+   
+    title: "Blinding Lights",
+   
+    artist: "The Weeknd",
+   
+    duration: "3:20",
+   
+    liked: true,
+   
+    audio:
+   
+    "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview221/v4/d6/87/41/d687410c-4714-af1d-e055-3296d4777226/mzaf_3899967882325561494.plus.aac.ep.m4a",
+   
+    image:
+   
+    "https://is1-ssl.mzstatic.com/image/thumb/Music125/v4/6f/bc/e6/6fbce6c4-c38c-72d8-4fd0-66cfff32f679/20UMGIM12176.rgb.jpg/100x100bb.jpg",
+   
+    },
